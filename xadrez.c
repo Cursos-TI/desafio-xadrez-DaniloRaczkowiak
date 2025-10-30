eb561522f35c7f5c2516244aa95a9ab977dbfbf6
DaniloRaczkowiak <contato@dlautomacoes.com.br>
2025-10-29 23:05:18 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
+int main (){
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+//começo dadescrição do codigo
+printf("Movimentando peças de chadrez!!!\n");
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+//comando for para movimentação do bispo
+    printf("movimentando o BISPO:\n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    for(int i =1; i<=5; i++){
+    printf("CIMA, DIREITA\n",i);
+    }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+//comando for para movimentação da torre
+    printf("movimentando a TORRE:\n");
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    for(int i =1; i<=5; i++){
+    printf("DIREITA\n",i);
+    }
 
-    return 0;
+    
+//comando for para movimentação da rainha
+    printf("movimentando a RAINHA\n");
+
+    for(int i =1; i<=8; i++){
+    printf("ESQUERDA\n",i);
+    }
+
+
+    printf("final da execução do codigo!");
+
+return 0;
 }
