f12503729b8ba5340ba694de8eb0732dc2106d22
DaniloRaczkowiak <contato@dlautomacoes.com.br>
2025-10-30 23:48:42 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main (){
 
+    
 //começo dadescrição do codigo
 printf("Movimentando peças de chadrez!!!\n");
 
@@ -12,6 +13,7 @@ printf("Movimentando peças de chadrez!!!\n");
     for(int i =1; i<=5; i++){
     printf("CIMA, DIREITA\n",i);
     }
+printf("\n");
 
 //comando for para movimentação da torre
     printf("movimentando a TORRE:\n");
@@ -21,14 +23,34 @@ printf("Movimentando peças de chadrez!!!\n");
     printf("DIREITA\n",i);
     }
 
+printf("\n");
     
 //comando for para movimentação da rainha
-    printf("movimentando a RAINHA\n");
+    printf("movimentando a RAINHA:\n");
 
     for(int i =1; i<=8; i++){
     printf("ESQUERDA\n",i);
     }
 
+    printf("\n");
+
+        printf("MOVIMENTAÇÃO CAVALO:\n");
+
+        //for externo while
+        // externo while
+        int m =1;
+         while ( m--)
+          {
+
+            
+            // interno for
+            for(int j = 0; j <=1; j++){
+
+                printf("BAIXO\n");                
+            }
+
+            printf("ESQUERDA\n\n");
+        }
 
     printf("final da execução do codigo!");
 
