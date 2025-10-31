f84e5d97770ef362ff6d642dc1b28b574315e4f7
DaniloRaczkowiak <contato@dlautomacoes.com.br>
2025-10-31 16:41:44 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
+
+//COMANDOS REF. A RECURSIVIDADE
+    void movertorre(int casast) {
+    if (casast > 0) {
+        printf("DIREITA\n");
+        movertorre(casast -1);
+    }
+}
+    void moverbispo(int casast) {
+    if (casast > 0) {
+        printf("CIMA,DIREITA\n");
+        moverbispo(casast -1);
+    }
+}
+    void moverrainha(int casast) {
+    if (casast > 0) {
+        printf("ESQUERDA\n");
+        moverrainha(casast -1);
+    }
+
+}
+
+
 int main (){
 
     
@@ -6,8 +29,8 @@ int main (){
 printf("Movimentando peças de chadrez!!!\n");
 
 
-//comando for para movimentação do bispo
-    printf("movimentando o BISPO:\n");
+//comando for para movimentação do bispo loop
+    printf("movimentando o BISPO LOOP:\n");
 
 
     for(int i =1; i<=5; i++){
@@ -16,7 +39,7 @@ printf("Movimentando peças de chadrez!!!\n");
 printf("\n");
 
 //comando for para movimentação da torre
-    printf("movimentando a TORRE:\n");
+    printf("movimentando a TORRE LOOP:\n");
 
 
     for(int i =1; i<=5; i++){
@@ -26,7 +49,7 @@ printf("\n");
 printf("\n");
     
 //comando for para movimentação da rainha
-    printf("movimentando a RAINHA:\n");
+    printf("movimentando a RAINHA LOOP:\n");
 
     for(int i =1; i<=8; i++){
     printf("ESQUERDA\n",i);
@@ -51,6 +74,18 @@ printf("\n");
 
             printf("ESQUERDA\n\n");
         }
+
+    // RECURSVIDADE TORRE/BISPO/RAINHA
+    printf("movimentando a Torre RECURSIVIDADE:\n");
+     movertorre(5);
+
+        printf("\n");
+    printf("movimentando a Bispo RECURSIVIDADE:\n");
+    moverbispo(5);
+
+        printf("\n");
+    printf("movimentando a RAINHA RECURSIVIDADE:\n");
+    moverrainha(8);
 
     printf("final da execução do codigo!");
 
